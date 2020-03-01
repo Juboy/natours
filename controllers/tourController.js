a9cabbccc0d6b7c63ce5c460b6ddcf2d5cbb5650
Juboy <aderibolejuwon@yahoo.com>
2020-03-02 01:41:49 +0300

    Home

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,17 +2,34 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
+    //1) Filtering
     const queryObj = { ...req.query }; //creating a copy of req.query with es6
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach(el => delete queryObj[el]);
     // console.log(req.query, queryObj); the excludedFields has been removed from queryObj
-    const tours = await Tour.find(queryObj);
-    // const tours = await Tour.find()
+
+    //2) Advance filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`); //using reqexp to add $ to gte, gt, lte, lt
+
+    console.log(JSON.parse(queryStr));
+
+    let query = Tour.find(JSON.parse(queryStr)); // retutns a query object
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      console.log(sortBy);
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt'); //the - sign means descending order
+    }
+    // const tours = Tour.find()
     //   .where('duration')
     //   .lt(5)
     //   .where('difficulty')
     //   .equals('easy');
-    //lt = lessthan***
+    //lt = lessthan*** // also returns a query object
+
+    const tours = await query; //runs the query
     res.status(200).json({
       status: 'success',
       result: tours.length,
